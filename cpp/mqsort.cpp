113d726caaf060da4e0cec866d4b02b0667f1d94
Gopal <46110945+GopalSir@users.noreply.github.com>
2020-06-23 21:15:10 +0530

    quick sort now works

--- cpp/mqsort.cpp
+++ cpp/mqsort.cpp
@@ -3,39 +3,40 @@
 int mpartition(std::vector<int>&arr,int mstart,int mend)
 {
 	
-	if(mstart>=mend)
-		return -1;
+	
 
 	int pivelem = arr[mend];
-	int mi=-1;
+	int mi=mstart-1;
 	
 	for(int j=mstart;j<mend;++j)
 	{
 	   if(arr[j]<=pivelem)
 	   {
 		   int temp=arr[j];
-		   arr[j]=arr[++i];
-		   arr[i]=temp;
+		   arr[j]=arr[++mi];
+		   arr[mi]=temp;
 	   }
 
 	}
-	temp=arr[++i];
-	arr[i]=arr[mend];
+	int temp=arr[++mi];
+	arr[mi]=arr[mend];
 	arr[mend]=temp;
+
+	return mi;
 
 }
 
-int mqsort(std::vector<int> &arr,int mtsart,int mend)
+int mqsort(std::vector<int> &arr,int mstart,int mend)
 {
-	if(arr.size()==1)
-		return 1;
+	 if(mstart>=mend)
+	 return 1;
 		
-	int pivot = mqsort(arr,mstart,mend);
+	int pivot = mpartition(arr,mstart,mend);
 	
 	if(pivot == -1)
 	return -1;
 
-	if(mqsort(arr,mstart,pivot) && mqsort(arr,pivot+1,end) )
+	if(mqsort(arr,mstart,pivot-1) && mqsort(arr,pivot+1,mend) )
 		return 1;
 	else 
 		return -1;
