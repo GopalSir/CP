907ed937749afe2b3d76c020c69192f12d55bc3b
unknown <46110945+GopalSir@users.noreply.github.com>
2020-08-19 19:19:02 +0530

    implemented breadth first search

--- source.cpp
+++ source.cpp
@@ -18,86 +18,100 @@ struct customcompare{
 
 //function to find shortest path to 1 to N vertices from a soucrce vertex
 //graph size should already be (N+1) 
-void dijkstra(vector<list<pair<int,int> > > &graph,int src_vert, vector<int> &short_path)
-{
+/** short_path is the vector which will get result of dijkstra algorithm **/
+void dijkstra(vector<list<pair<int,int> > > &graph,int src_vert, vector<int> &short_path){
     priority_queue<pair<int,int>, vector<pair<int,int> >,customcompare >vert_queue;
-
     vector<bool>visited(graph.size(),false);      //visited vector
-
     short_path.resize(graph.size());        //making sure to correct any size related issue
-
     for(auto &elem: short_path)
      elem = INT32_MAX;          // all distance infinite initially
-
      short_path[src_vert] = 0;  //distance of source vertex from itself is zero duh       
-
-     vert_queue.push(make_pair(src_vert,short_path[src_vert]));
-      
+     vert_queue.push(make_pair(src_vert,short_path[src_vert]));      
      while(vert_queue.empty()!=true)
      {
-         int cur_vertex = vert_queue.top().first;
-         
-         
+         int cur_vertex = vert_queue.top().first;        
                      //the vertex popped is the now visited and done for
 
          vert_queue.pop();                    //pop the visited vertex out of queue
          if(visited[cur_vertex]==true) continue ;
-
          visited[cur_vertex]=true;
-
          for(auto adj_vert : graph[cur_vertex])
-         {
+                  {
                if(short_path[adj_vert.first]>(short_path[cur_vertex] + adj_vert.second )) 
-                short_path[adj_vert.first] = short_path[cur_vertex] + adj_vert.second;
-                
+                short_path[adj_vert.first] = short_path[cur_vertex] + adj_vert.second;                
                 if(visited[adj_vert.first]==false)
                 {
                     vert_queue.push(make_pair(adj_vert.first,short_path[adj_vert.first]));                  
-                }
-         }        
-         
+                }         }           
      } 
 
 
 }
 
+/**
+ * saves the bfs traversal of graph in bfs_path vector
+ * src_vert = starting vertex  **/
+void breadth_first_search(vector<list<pair<int,int> > > &graph,int src_vert, vector<int> &bfs_path)
+{
+    
+
+        vector<bool>visited(graph.size(),false);
+        queue<int>vert_queue;       //vertex number
+        
+
+        visited[src_vert] = true;
+        vert_queue.push(src_vert);         //pushig source vertex
+
+    while (!vert_queue.empty())
+    {
+        int cur_vertex = vert_queue.front();
+
+       
+
+        vert_queue.pop();
+        visited[cur_vertex] = true;
+        bfs_path.push_back(cur_vertex);
+        for(auto &adj_vertex : graph[cur_vertex])
+        {
+            if(visited[adj_vertex.first] == false)         //only process unvisited vertices
+            {
+                vert_queue.push(adj_vertex.first);
+                visited[adj_vertex.first] = true;
+            }            
+        }
+
+        
+
+    }          
+}
 
 
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
 int main()
-{  
+{
     vector<list<pair<int,int> > >graph;
-    graph.resize(7);      //N+1;
-       graph[1].push_back(make_pair(2,-1));
-       // graph[2].push_back(make_pair(1,-1));
-       
-       graph[1].push_back(make_pair(3,20));
-       graph[3].push_back(make_pair(1,20));  
-       
-        graph[2].push_back(make_pair(3,10));  
-         graph[3].push_back(make_pair(2,10));  
-       
-         graph[2].push_back(make_pair(4,7));
-         graph[4].push_back(make_pair(2,7));
+    int n;    
+    cin>>n;
+    graph.resize((4*n)+1);              //graph size will be N+1
+    
+   // graph[0].push_back({0,0});
 
-         graph[3].push_back(make_pair(4,2));
-         graph[4].push_back(make_pair(4,2));
-
-       vector<int>dis(graph.size());
-       dijkstra(graph,1,dis);
-       for(auto  &elem : dis)
-       cout<<elem<<"\t";
-    //    cout<<endl;
-    // priority_queue<pair<int,int>, vector<pair<int,int> >,customcompare >vert_queue;
-    // vert_queue.push(make_pair(1,10));
-    // vert_queue.push(make_pair(2,3));
-    // vert_queue.push(make_pair(3,6));
-    // while(vert_queue.empty()!=true)
-    // {
-    //     cout<<vert_queue.top().first<<"\t";
-    //     vert_queue.pop();
-    // }
+    for(int i=1;i<=(2*n);++i)
+    {
+        graph[i].push_back({2*i,0});    //reachable vertex and it's weight (weight is irrelevant here)
+        graph[i].push_back({i-1,0});
+    }
+    vector<int>ans;
+    breadth_first_search(graph,1,ans);
+    for(auto x:ans)
+    cout<<x<<"\t";
 
 
+     
+ 
 }
 
 
